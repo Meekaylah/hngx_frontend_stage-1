251eb78a64dc67dfb767f9da29166811d50c133a
Meekaylah <kaylahusanga@gmail.com>
2023-09-10 20:03:43 +0100

    Update on script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function updateDayOfTheWeek() {
     const currentDate = new Date();
     const dayOfTheWeekNumber = currentDate.getDay();
-    const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     const dayName = dayNames[dayOfTheWeekNumber];
 
     const utcTime = currentDate.getTime();
 
     const dayOfTheWeekElement = document.getElementById('currentDayOfTheWeek');
-    dayOfTheWeekElement.textContent = `${dayName}`;
+    dayOfTheWeekElement.textContent = dayName;
     const utcTimeElement = document.getElementById('currentUTCTime');
-    utcTimeElement.textContent = `${utcTime}`
+    utcTimeElement.textContent = utcTime;
 }
 
 updateDayOfTheWeek();
